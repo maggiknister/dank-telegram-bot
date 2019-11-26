3eccf16395a9feacc3db77fa13f17a83e476abe7
Jan-Magnus Kister <janmagnuskister@gmail.com>
2019-11-26 11:38:48 +0100

    feat(deployment): Füge Benutzung von fs in settings.js hinzu

--- settings/settings.js
+++ settings/settings.js
@@ -1,5 +1,9 @@
 let fs = require('fs');
-let settingsFile = fs.readFileSync('settings/settings.json');
+let path = require('path');
+
+let settingsFilePath = path.join(__dirname, 'settings.json');
+
+let settingsFile = fs.readFileSync(settingsFilePath);
 let settings = JSON.parse(settingsFile);
 
 module.exports = settings;
